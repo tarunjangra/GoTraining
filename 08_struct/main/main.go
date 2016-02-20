46effce87bf00b7402e4e942a8094c0aece66467
Tarun Jangra <tarun@izap.in>
2016-02-19 19:47:41 -0800

    More better structures

--- 08_struct/main/main.go
+++ 08_struct/main/main.go
@@ -7,13 +7,29 @@ import (
 func main() {
 	var tom Person
 	tom.Age, tom.Name = 33,"tarun"
-	fmt.Printf("Name: %s, Age: %d", tom.Name, tom.Age)
+	fmt.Printf("Name: %s, Age: %d\n", tom.Name, tom.Age)
 
 	r1 := Rectangle{12, 2}
-	//r2 := Rectangle{9, 4}
 	c1 := Circle{10}
-	//c2 := Circle{25}
 
 	fmt.Println("Area of r1 is: ", r1.Area())
 	fmt.Println("Area of c1 is: ", c1.Area())
+
+	boxes := BoxList{
+		Box{4,4,4,RED},
+		Box{10,10,10,YELLOW},
+		Box{14,41,24,BLACK},
+		Box{49,46,44,BLUE},
+		Box{84,74,6,WHITE},
+		Box{24,34,44,YELLOW},
+		Box{45,43,41,RED},
+	}
+
+	fmt.Printf("We have %d boxes in our set\n", len(boxes))
+	fmt.Println("The volume of the first one is", boxes[0].Volume(),"cm3")
+	fmt.Println("the color of hte last one is",boxes[len(boxes)-1].Color.String())
+	fmt.Println("The biggest one is", boxes.BiggestsColor().String())
+	boxes.PaintItBack()
+	fmt.Println("The color of the second one is", boxes[1].Color.String())
+	fmt.Println("Obviously the color of the biggest one is", boxes.BiggestsColor().String())
 }
--- 08_struct/strctures/st1.go
+++ 08_struct/strctures/st1.go
@@ -28,3 +28,51 @@ func (r Rectangle) Area() float64 {
 func (c Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
 }
+
+const(
+	WHITE = iota
+	BLACK
+	BLUE
+	RED
+	YELLOW
+)
+
+type Color byte
+
+type Box struct {
+	Width, Height, Depth float64
+	Color Color
+}
+
+type BoxList []Box // a slice of boxes
+
+func (b Box) Volume() float64 {
+	return b.Width * b.Height * b.Depth
+}
+
+func (b *Box) SetColor(c Color) {
+	b.Color = c
+}
+
+func (b1 BoxList) BiggestsColor() Color {
+	v := 0.00
+	k := Color(WHITE)// default value of each color would be WHITE
+	for _,b := range b1 {
+		if b.Volume() > v {
+			v = b.Volume()
+			k = b.Color
+		}
+	}
+	return k
+}
+
+func (b1 BoxList) PaintItBack() {
+	for i, _ := range b1 {
+		b1[i].SetColor(BLACK)
+	}
+}
+
+func (c Color) String() string {
+	strings := []string {"WHITE","BLACK","BLUE","RED","YELLOW"}
+	return strings[c]
+}
